a0ac4d7001c103cf5f3555a9af843c72715b910f
Zstanford1110 <zachary.s1110@gmail.com>
2024-03-12 15:58:44 -0400

    Start of a major overhaul

--- frontend/src/pages/homePage.tsx
+++ frontend/src/pages/homePage.tsx
@@ -1,29 +1,43 @@
 import { extractAccessToken, logout } from "../utils/authUtils";
 import { Link, useLoaderData } from "react-router-dom";
-import { getTopArtists } from "../utils/spotifyUtils";
-import { TopArtists } from "../types/spotifyTypes";
+import { getTopArtists, spotifyRequest } from "../utils/spotifyUtils";
+import { TopArtists, UserProfile } from "../types/spotifyTypes";
 
 export async function homePageDataLoader() {
     const accessToken = extractAccessToken();
-    const [topArtistData1, topArtistData2] = await Promise.all([
+    const [topArtistData1, topArtistData2, topArtistData3, topArtistDat4] = await Promise.all([
         getTopArtists(accessToken, 50, 0),
-        getTopArtists(accessToken, 50, 49)
+        getTopArtists(accessToken, 50, 50),
+        getTopArtists(accessToken, 50, 100),
+        getTopArtists(accessToken, 50, 150), // Confirmed up to 200 so far
     ]);
 
+    // Strip this entire data loader
+    // Build a util function that fetches as many top artists and tracks as possible using loops
+    // Perform this fetch once and cache the data (don't overload the API)
+    // rate limiting between the API calls?
+    // set a limit, like maybe it's only relevant to check their top 500 artists and sort them over the decades
+    // Use util function here to concisely retrieve artist and track data
+
     // Cannot set offsets over 50, so we need to remove the last item from the first request to avoid duplicates
-    const topArtistData1DuplicateRemoved = topArtistData1.items.slice(0, -1);
+    // Shouldn't need this anymore if my offsets are correct
+    // const topArtistData1DuplicateRemoved = topArtistData1.items.slice(0, -1);
 
     const combinedTopArtistData = {
-        items: [...topArtistData1DuplicateRemoved, ...topArtistData2.items],
-        total: topArtistData1.total + topArtistData2.total
+        items: [...topArtistData1.items, ...topArtistData2.items, ...topArtistData3.items, ...topArtistDat4.items],
+        total: topArtistData1.total + topArtistData2.total + topArtistData3.total + topArtistDat4.total,
     };
 
-    return combinedTopArtistData;
+    const profileData = await spotifyRequest("https://api.spotify.com/v1/me", accessToken);
+
+    return { combinedTopArtistData, profileData };
 }
 
 
 export function HomePage() {
-    const topArtistData = useLoaderData() as TopArtists;
+    const fetchedData = useLoaderData() as { combinedTopArtistData: TopArtists, profileData: UserProfile };
+    const topArtistData = fetchedData.combinedTopArtistData;
+    const profileData = fetchedData.profileData;
 
     const handleLogout = () => {
         logout();
@@ -36,7 +50,7 @@ export function HomePage() {
     return (
         <div>
             <h1>Home Page</h1>
-            <p>Welcome "User"!</p>
+            <p>Welcome {profileData.display_name}!</p>
             <Link to="/profile">
                 <button>Go to Profile</button>
             </Link>
