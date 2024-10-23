604d82386753418aefe2d50a3569e3f37f6ed0a8
Zachary Stanford <zachary.s1110@gmail.com>
2024-10-22 21:48:00 -0400

    Updated React, installed Million Lint

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import MillionLint from "@million/lint"
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
   return {
-    plugins: [react()],
+    plugins: [
+      react(),
+      MillionLint.vite(),
+    ],
   }
 })
